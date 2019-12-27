fbcade4e5d7cac14ef3c5ff161dfa39dce96a699
mineyev2 <mineyev2@illinois.edu>
2019-12-27 14:32:19 -0600

    import image picker wors after linking the module to the project

---
+++ ios/build/Spoterator/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/react-native-image-picker.build/Objects-normal/x86_64/ImagePickerManager.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/romanmineyev/Spoterator/node_modules/react-native-image-picker/ios/ImagePickerManager.m \
+  /Users/romanmineyev/Spoterator/ios/Pods/Target\ Support\ Files/react-native-image-picker/react-native-image-picker-prefix.pch \
+  /Users/romanmineyev/Spoterator/node_modules/react-native-image-picker/ios/ImagePickerManager.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTBridgeModule.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTDefines.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTInspectorDevServerHelper.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTPackagerConnection.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTConvert.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTAnimationType.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTBorderStyle.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTLog.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTAssert.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTUtils.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTPointerEvents.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/React-Core/React/RCTTextDecorationLineType.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/Yoga/yoga/Yoga.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/Yoga/yoga/YGEnums.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/Yoga/yoga/YGMacros.h \
+  /Users/romanmineyev/Spoterator/ios/Pods/Headers/Public/Yoga/yoga/YGValue.h
--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import ImagePicker from 'react-native-image-picker'
 
 export default function CreateScreen() {
     return (
